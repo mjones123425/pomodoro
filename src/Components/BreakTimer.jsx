a4d84c242e18a73f4877f9cb984b3765e08054c4
Kenta Lange <59261619+KentaLange@users.noreply.github.com>
2025-05-03 17:55:47 -0500

    Unfinished 5/3

--- src/Components/BreakTimer.jsx
+++ src/Components/BreakTimer.jsx
@@ -1,1 +1,22 @@
 // page for the break time window
+import Login from './Login'
+import axios from 'axios'
+import React, {useState} from 'react';
+//import Home from './Users/kl/Shin_Pomodoro/pomodoro-main/src/Components/Home'
+import { BrowserRouter as Router, Link,useNavigate } from 'react-router-dom';
+// page for the break time window
+const BreakTime = () => {
+    return(
+    <div>
+       
+       
+        <h1>Break Time!</h1>
+        <Link to="/Home">
+                  <button type="button" class='skip'>Back to Home menu</button>
+        </Link>
+        <h2>↓Suggestions for your break↓</h2>
+        <a href="YOUR_NETFLIX_URL" target="https://www.netflix.com/browse">Netflix</a>
+    </div>
+    );
+}
+export default BreakTime
