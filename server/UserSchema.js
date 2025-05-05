d3a7e526de168f4ed866c7355289a54214d872e7
Johne83 <142929564+Johne83@users.noreply.github.com>
2025-05-05 13:17:46 -0400

    Update UserSchema.js

--- server/UserSchema.js
+++ server/UserSchema.js
@@ -1,10 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); 
+
 const UserSchema = new mongoose.Schema({
     f_name: String,
     l_name: String, 
     username: String,
-    password: String
+    password: String,
+    email: String,
+    blacklist: [String],
+    whitelist: [String],
+    greylist: [String],
+    toDoLists: [String],
+    toDoList: Boolean,
+    blackOrWhite: Boolean
 });
+
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
